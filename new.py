175a344e6c5c89063ccb237451f13ea7352f6a81
zrx2gg6vq8-tech <zrx2gg6vq8@privaterelay.appleid.com>
2025-11-28 19:30:55 +0100

    Add files via upload

---
+++ new.py
@@ -0,0 +1,9 @@
+from operator import truediv
+a=5
+print("this is integer", type(a))
+l=3.5
+print("it is float" , type(l))
+b="hello"
+print("this is string" , type(b))
+C=True
+print("this is booling" , type(C))
